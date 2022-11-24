a04434dc009034fef189b7ed5f89d4eb2f2500cd
Edvinas1982 <79442331+Edvinas1982@users.noreply.github.com>
2022-11-24 15:54:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// UŽDUOTIS
+// Parašykite programą, kuri turėtų du kintamuosius, saugančius stačiakampio pločio
+// ir aukščio reikšmes.
+// Panaudodami matematines operacijas ir duotus kintamuosius apskaičiuokite
+// stačiakampio perimetrą, plotą ir įstrižainę.
+// Pasirinktu būdu atspausdinkite pradinius stačiakampio duomenis (plotį ir aukštį) ir
+// suskaičiuotus rezultatus.
+// p.s. programa rašoma html faile
+// ✶✶) Raskite būdą kaip rezultatą išvesti ne tik į concole, bet ir kaip kažkokio
+// puslapyje esančio <div> elemento reikšmę
+
+// staciakampio plotis
+var plotis = 5;
+
+// staciakampio ilgis
+var ilgis = 10;
+
+// staciakampio perimetras
+var perimetras = 2 * (plotis + ilgis);
+console.log('Staciakampio perimetras yra lygus = ' + perimetras);
+
+// staciakampio plotas
+var plotas = plotis * ilgis;
+console.log('Staciakampio plotas yra lygus = ' + plotas);
---
+++ readme.md
@@ -0,0 +1,10 @@
+UŽDUOTIS
+Parašykite programą, kuri turėtų du kintamuosius, saugančius stačiakampio pločio
+ir aukščio reikšmes.
+Panaudodami matematines operacijas ir duotus kintamuosius apskaičiuokite
+stačiakampio perimetrą, plotą ir įstrižainę.
+Pasirinktu būdu atspausdinkite pradinius stačiakampio duomenis (plotį ir aukštį) ir
+suskaičiuotus rezultatus.
+p.s. programa rašoma html faile
+✶✶) Raskite būdą kaip rezultatą išvesti ne tik į concole, bet ir kaip kažkokio
+puslapyje esančio <div> elemento reikšmę
